f5e44b9ae6f5d95014f4a89af51f0e21fcfdc61d
kilo <pipianino@protonmail.com>
2022-04-21 19:56:34 +0600

    added Text editor in BuilderCV

--- routes/builder.js
+++ routes/builder.js
@@ -7,6 +7,7 @@ const app = express();
 app.get('/builder',(req, res, next) => {
     res.render(__dirname+'views/builder')
 })
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 router
     .route("/")
     .get((req, res) => res.render(path.resolve('views/builder.ejs')))
