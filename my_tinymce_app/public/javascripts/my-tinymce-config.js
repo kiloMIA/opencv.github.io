71b4113ab7e97e07f9d06ea63161a1a4ad21300d
kilo <pipianino@protonmail.com>
2022-04-22 14:57:06 +0600

    Bootstrap fixed

--- my_tinymce_app/public/javascripts/my-tinymce-config.js
+++ my_tinymce_app/public/javascripts/my-tinymce-config.js
@@ -1,4 +1,18 @@
 tinymce.init({
-    selector: 'textarea#my-expressjs-tinymce-app',
-    plugins: 'lists link image table code help wordcount'
+    selector: '#myTextarea',
+    width: 600,
+    height: 300,
+    plugins: [
+        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+        'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
+        'media', 'table', 'emoticons', 'template', 'help'
+    ],
+    toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
+        'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+        'forecolor backcolor emoticons | help',
+    menu: {
+        favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
+    },
+    menubar: 'favs file edit view insert format tools table help',
+    content_css: 'css/content.css'
 });
