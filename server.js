332298b38e4008b49b755f7545d6cf0201a0dd73
kilo <pipianino@protonmail.com>
2022-05-09 10:30:26 +0600

    minor changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.set('view engine','ejs')
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+"/public"))
+app.set("public", path.join(__dirname, "public"));
 
 app.use("/", require("./routes/root"));
 app.use("/open", require("./routes/open"));
